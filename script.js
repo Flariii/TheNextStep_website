c148b0d9a6912d6720162bd060c7e1adc7262fa1
Flariii <homa4467@gmail.com>
2024-11-05 18:16:19 +0200

    add new  functions

--- script.js
+++ script.js
@@ -11,8 +11,11 @@
     // Створюємо мітки та зберігаємо їх в об'єкті
     var markers = {
         'marker1': L.marker([49.83098666231625, 23.98874194574129]).addTo(map).bindPopup('Impero Cafe'),
-        'marker2': L.marker([49.8500, 24.0155]).addTo(map).bindPopup('Mountain House - 1.500$'),
-        'marker3': L.marker([49.8600, 24.0400]).addTo(map).bindPopup('Beach Villa - 2.000$')
+        'marker2': L.marker([49.82740, 23.99807]).addTo(map).bindPopup('Mokka 1554'),
+        'marker3': L.marker([49.84172, 24.03280]).addTo(map).bindPopup('Львівська копальня кави'),
+        'marker4': L.marker([49.84173, 24.02347]).addTo(map).bindPopup('Cate Cafe'),
+        'marker5': L.marker([49.84363, 24.03216]).addTo(map).bindPopup('Tante Sophie Càfe Escargot'),
+        'marker6': L.marker([49.84161, 24.03012]).addTo(map).bindPopup('На бамбетлі')
     };
 
     // Функція для фокусування на мітці при натисканні на посилання
