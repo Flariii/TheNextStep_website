19cb3260a98aeef03b9d7e27addf1c3c1e8c05f4
Andrew Kononenko <wastardy.k@gmail.com>
2024-11-14 20:58:03 +0200

    try another way to set api url

--- public/script.js
+++ public/script.js
@@ -9,7 +9,7 @@ async function loadPosts() {
     }
 
     try {
-        const response = await fetch(`https://thenextstep-api-1.onrender.com/posts/${type}`); 
+        const response = await fetch(`https://thenextstepapi.onrender.com/posts/${type}`); 
         const posts = await response.json();  // Перетворюємо відповідь на JSON
 
         const container = document.getElementById('places-container');
